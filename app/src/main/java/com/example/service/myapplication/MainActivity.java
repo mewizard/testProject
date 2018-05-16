ab189240979d3ea666f1092203bfad0ec733072b
mewizard <wizard_club@hotmail.com>
2018-05-17 00:02:06 +0900

    - beta02

--- app/src/main/java/com/example/service/myapplication/MainActivity.java
+++ app/src/main/java/com/example/service/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("TEST", "beta01");
+        Log.i("TEST", "beta02");
     }
 }
